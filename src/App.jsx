0a0c6478e40dd7029a6dd0c3ab1c63d58bd3d801
Iris Lee <irislee4git@gmail.com>
2024-05-21 15:57:43 -0400

    through ch 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,90 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.css"
 import { submitJoke } from "./services/jokeService.jsx"
+import stevePic from "./assets/steve.png"
+import { getAllJokes } from "./allJokes.jsx"
+
 
 export const App = () => {
   const [joke, setJoke] = useState('')
+  const [allJokes, setAllJokes] = useState([])
+  const [untoldJokes, setUntoldJokes] = useState([])
+  const [toldJokes, setToldJokes] = useState([])
 
-  const handleChange = (joke) => {
-    setJoke(joke.target.value)
+  const handleChange = (event) => {
+    setJoke(event.target.value)
   }
 
+  //for all jokes
+  useEffect(() => {
+    getAllJokes().then((jokesArray) => {
+      setAllJokes(jokesArray)
+      console.log("jokes set!")
+    }) 
+  }, [])
+
+//filter for untold jokes
+    useEffect(() => {
+      if (untoldJokes) {
+        const untoldJokesArray = allJokes.filter(
+          (joke) => joke.told === false
+        )
+        setUntoldJokes(untoldJokesArray)
+    }}, [untoldJokes, allJokes])
+
+//filter for told jokes
+useEffect(() => {
+  if (toldJokes) {
+    const toldJokesArray = allJokes.filter(
+      (joke) => joke.told === true
+    )
+    setToldJokes(toldJokesArray)
+}}, [toldJokes, allJokes])
+
   return <>
-    <form
-      method="post"
-      onSubmit={submitJoke}>
+    <div>
+      <div className="app-heading-circle">
+        <img className="app-logo" src={stevePic} alt="Good job Steve" />
+      </div>
+        <header className="app-heading-text">Chuckle Checklist</header>
         <input
-      className=""
+      className="joke-add-form"
       type="text"
-      placeholder="New One Liner"
+      placeholder="Add Joke"
       value={joke}
       onChange={handleChange}
-    />
-    <button className="joke-input-submit"
-            type="submit"
-            >
-            Submit Joke
-            </button>
-    </form> 
+      />
+      {/* add .then to render page with empty string upon clicking button */}
+      <button onClick = { () => {submitJoke({text: joke, told: false}).then(setJoke(""))}}
+              className="joke-input-submit"
+              type="submit"
+              >
+              Add
+              </button>
+      <div className="joke-lists-container">
+          <div className="joke-list-container">
+            {untoldJokes.map(joke => {
+              return (
+                <ul className="joke-list-item" key={joke.id}>
+                  <li className="joke-list-item-text">
+                    {joke.text}
+                  </li>
+              </ul>
+              )
+            })}
+          </div>
+          <div className="joke-list-container">
+            {toldJokes.map(joke => {
+              return (
+                <ul className="joke-list-item" key={joke.id}>
+                  <li className="joke-list-item-text">
+                    {joke.text}
+                  </li>
+              </ul>
+              )
+            })}
+          </div>
+      </div>
+    </div> 
       </>
 }
--- src/services/jokeService.jsx
+++ src/services/jokeService.jsx
@@ -1,11 +1,11 @@
-// export const getJokes = () => {
-//     return fetch('http://localhost:8088/jokes').then((res) => res.json())
-// }
+//joke below is an object being called and sent to database.json
+export const submitJoke = async (joke) => {
 
-export const submitJoke = (joke) => {
-    joke.preventDefault();
+    const newJoke = {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json'},
+        body: JSON.stringify(joke)
+    };
 
-    const form = joke.target;
-    const formData = new FormData(form);
-    fetch('http://localhost:8088/jokes', { method: form.method, body: formData.value});
+    await fetch ('http://localhost:8088/jokes', newJoke)
 }
